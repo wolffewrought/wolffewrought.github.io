4ebae4c7360b106191743f737e7688621b52e655
wolffewrought <wolffewrought@gmail.com>
2026-06-03 23:27:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'invoice-mgr-v7';
+const CACHE = 'invoice-mgr-v8';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
